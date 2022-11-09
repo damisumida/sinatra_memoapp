16a104d5f28f343913ca4736b24ec73e97b03dc2
damisumida <gjptmda628@gmail.com>
2022-11-09 14:03:31 +0900

    - メモ新規作成・更新時の遷移先画面を変更 - 戻るボタンを追加 - XSS対策を追加 - Gemfileを追加 - ｇitignoreを追加

--- myapp.rb
+++ myapp.rb
@@ -7,6 +7,12 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
 get '/' do
   @memos = load_data['memos']
   erb :top
@@ -17,40 +23,33 @@ get '/memo/compose' do
 end
 
 post '/memo/compose' do
-  title = params[:title]
-  memo = params[:memo]
   data = load_data
   id = calc_maxid(data)
-  newmemo = { 'id' => id.to_i, 'title' => title, 'memo' => memo }
+  newmemo = { 'id' => id.to_i, 'title' => params[:title], 'memo' => params[:memo] }
   data['memos'].push(newmemo)
-  File.open('db/data.json', 'w') do |file| 
-    file.write(JSON.generate(data))
-  end
-  redirect to "/"
+  write_data(data)
+  redirect to "/memo/#{id}"
 end
 
 get '/memo/:id' do
-  @id = params[:id]
-  @title = load_title(@id)
-  @memo = load_memo(@id)
+  id = params[:id]
+  @memo = load_memo(id)
+  p @memo
   erb :showmemo
 end
 
 get '/memo/:id/edit' do
-  @id = params[:id]
-  @title = load_title(@id)
-  @memo = load_memo(@id)
+  id = params[:id]
+  @memo = load_memo(id)
   erb :editmemo
 end
 
 patch '/memo/:id' do
   rewrit_memo = { 'id' => params[:id].to_i, 'title' => params[:title], 'memo' => params[:memo] }
-  rewrite_memos = create_rewrite_memos(id, rewrit_memo)
-  File.open('db/data.json', 'w') do |file|
-    file.write(JSON.generate(rewrite_memos))
-  end
+  rewrite_memos = create_rewrite_memos(params[:id], rewrit_memo)
+  write_data(rewrite_memos)
   @memos = rewrite_memos['memos']
-  redirect to "/"
+  redirect to "/memo/#{params[:id]}"
 end
 
 delete '/memo/:id' do
@@ -59,9 +58,7 @@ delete '/memo/:id' do
   data['memos'].each do |memo|
     data['memos'].delete(memo) if id.to_i == memo['id']
   end
-  File.open('db/data.json', 'w') do
-    |file| file.write(JSON.generate(data))
-  end
+  write_data(data)
   redirect to "/"
 end
 
@@ -82,28 +79,25 @@ def load_data
   data
 end
 
-def load_title(id)
-  title = ''
-  data = load_data
-  data['memos'].each do |item|
-    title = item['title'] if item['id'] == id.to_i
-  end
-  title
-end
-
 def load_memo(id)
-  memo = ''
+  memo = ""
   data = load_data
-  data['memos'].each do |item|
-    memo = item['memo'] if item['id'] == id.to_i
+  data['memos'].each do |m|
+    memo = m if m['id'] == id.to_i
   end
   memo
 end
 
 def create_rewrite_memos(id, rewrit_memo)
   data = load_data
-  data['memos'].each do |memo, i|
+  data['memos'].each do |memo, i=0|
     data['memos'][i+1] = rewrit_memo if id.to_i == memo['id']
   end
   data
+end
+
+def write_data(data)
+  File.open('db/data.json', 'w') do |file|
+    file.write(JSON.generate(data))
+  end
 end
